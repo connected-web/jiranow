d24547c93591dd7f93ceb410a11771fac72fc51d
John Beech <jbeech@marqeta.com>
2021-11-11 16:47:54 +0000

    Implement file readers for folder, package, and system config overrides

--- src/models/packageConfig.js
+++ src/models/packageConfig.js
@@ -1,10 +1,30 @@
+const asyncFs = require('fs/promises')
+const path = require('path')
+const clone = require('../util/clone')
 const workingConfig = require('./workingConfig')
 
-const localFilepath = 'package.json'
+const localFilepath = path.resolve('./package.json')
 const localProperty = 'jiranow'
 
 async function create () {
-  return workingConfig.create()
+  const base = await workingConfig.create()
+
+  let config = {}
+  let text = '{}'
+
+  try {
+    text = await asyncFs.readFile(localFilepath, 'utf8')
+  } catch (ex) {
+    console.debug(`Unable to load ${localFilepath}.`)
+  }
+
+  try {
+    config = JSON.parse(text)
+  } catch (ex) {
+    console.error(`Found file; but unable to parse ${localFilepath} as JSON:`, ex.message)
+  }
+
+  return Object.assign({}, clone(base), config[localProperty] || {})
 }
 
 module.exports = {
--- src/models/systemConfig.js
+++ src/models/systemConfig.js
@@ -1,9 +1,30 @@
+const asyncFs = require('fs/promises')
+const clone = require('../util/clone')
+const resolveHome = require('../util/resolveHome')
 const workingConfig = require('./workingConfig')
 
-const homeDirectory = '~/.jiranow/jiranow.json'
+const localFilepath = resolveHome('~/.jiranow/jiranow.json')
+const localProperty = 'jiranow'
 
 async function create () {
-  return workingConfig.create()
+  const base = await workingConfig.create()
+
+  let config = {}
+  let text = '{}'
+
+  try {
+    text = await asyncFs.readFile(localFilepath, 'utf8')
+  } catch (ex) {
+    console.debug(`Unable to load ${localFilepath}.`)
+  }
+
+  try {
+    config = JSON.parse(text)
+  } catch (ex) {
+    console.error(`Found file; but unable to parse ${localFilepath} as JSON:`, ex.message)
+  }
+
+  return Object.assign({}, clone(base), config[localProperty] || config || {})
 }
 
 module.exports = {
